7ecb084d12340cbd2a913b7bbff2b7fd1c33530c
zcasqw92-yurist <zcasqw92@gmail.com>
2026-08-04 12:14:36 +0200

    Добавить дату статьи о скрытом дефекте автомобиля

--- site.config.mjs
+++ site.config.mjs
@@ -49,6 +49,7 @@ export const site = {
     "/razbory/vernut-dengi-za-otmenennuyu-bron-otelya": "2026-08-04",
     "/razbory/dengi-za-kontraktnyy-dvigatel-bez-raspiski": "2026-08-04",
     "/razbory/ozon-poteryal-tovar-ili-nachislil-lishnyuyu-logistiku": "2026-08-04",
+    "/razbory/skrytyy-defekt-poderzhannogo-avtomobilya": "2026-08-04",
     "/praktika": "2026-08-02",
     "/praktika/otmena-otkazov-policii-i-dopolnitelnaya-proverka": "2026-08-02",
     "/praktika/pretenziya-i-raschet-po-dolgu-po-raspiske": "2026-08-02",

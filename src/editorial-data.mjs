74011575c9f96ce4d52bc69a6ee05c38f467bfcb
zcasqw92-yurist <zcasqw92@gmail.com>
2026-08-05 21:25:12 +0200

    Register C-122 lost parcel article

--- src/editorial-data.mjs
+++ src/editorial-data.mjs
@@ -22,6 +22,7 @@ import { usedCarHiddenDefectArticles } from "./used-car-hidden-defect-data.mjs";
 import { medicalErrorFirstStepsArticles } from "./medical-error-first-steps-data.mjs";
 import { bailiffSmallPensionPaymentsArticles } from "./bailiff-small-pension-payments-data.mjs";
 import { designerInfographicExclusiveRightsArticles } from "./designer-infographic-exclusive-rights-data.mjs";
+import { yandexDeliveryLostParcelArticles } from "./yandex-delivery-lost-parcel-data.mjs";
 
 const usedCarHiddenDefectPublicationArticles = usedCarHiddenDefectArticles.map((article) => ({
   ...article,
@@ -39,6 +40,7 @@ export const articles = [
   ...medicalErrorFirstStepsArticles,
   ...bailiffSmallPensionPaymentsArticles,
   ...designerInfographicExclusiveRightsArticles,
+  ...yandexDeliveryLostParcelArticles,
 ];
 
 export const findArticleBySlug = (slug) => articles.find((item) => item.slug === slug);
